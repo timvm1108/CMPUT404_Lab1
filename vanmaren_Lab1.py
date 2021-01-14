6101b2aee655d9fe92b1d09cd28affb7b40c199c
Tim van Maren <timvanmaren@hotmail.com>
2021-01-13 18:06:37 -0700

    Changed Script to GET and Print its Code

--- vanmaren_Lab1.py
+++ vanmaren_Lab1.py
@@ -1,4 +1,9 @@
 import requests
 
 print(requests.__version__)
-print(requests.get("http://www.google.com/"))
+
+google_request = requests.get("http://www.google.com")
+print(google_request)
+
+git_request = (requests.get("https://raw.githubusercontent.com/timvm1108/CMPUT404_Lab1/main/vanmaren_Lab1.py"))
+print(git_request.text)
